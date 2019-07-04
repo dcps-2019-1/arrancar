977786037cb885367ac3a4b59508330d14ea2462
dhoyosg <dhoyosg@unal.edu.co>
2019-07-04 16:50:09 -0500

    Update ListaMantenimientos.blade.php

--- resources/views/empresa/ListaMantenimientos.blade.php
+++ resources/views/empresa/ListaMantenimientos.blade.php
@@ -9,7 +9,6 @@
                     <table class="table">
                         <thead>
                         <tr>
-                            <th scope="col">id</th>
                             <th scope="col">Placa</th>
                             <th scope="col">Fecha de entrada</th>
                             <th scope="col">Fecha de salida</th>
@@ -18,7 +17,6 @@
                         <tbody>
                         @foreach ($mantenimientos as $mantenimiento)
                             <tr>
-                                <td> {{ $mantenimiento->id }} </td>
                                 <td> {{ $mantenimiento->bus_id }} </td>
                                 <td> {{ $mantenimiento->fecha_entrada }} </td>
                                 <td> {{ $mantenimiento->fecha_salida }} </td>
