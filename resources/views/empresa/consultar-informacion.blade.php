8740e33e49191185ed45fbc01a66595887dc6555
Angel David Machado <amachadoa@unal.edu.co>
2019-07-04 19:10:22 -0500

    Update consultar-informacion.blade.php

--- resources/views/empresa/consultar-informacion.blade.php
+++ resources/views/empresa/consultar-informacion.blade.php
@@ -5,16 +5,18 @@
 
     <div class="row justify-content-center">
         <div class="col-6">
-            <div class="col text-center">
+            <div class="col text-center texto-inside">
                 <h2>Opciones de consulta</h2>
+                <br>
     <div class="list-group">
-        <a href="ListaConductores" class="list-group-item list-group-item-action">Consultar conductores</a>
-        <a href="ListaBuses" class="list-group-item list-group-item-action">Consultar buses</a>
-        <a href="ListaMantenimientos" class="list-group-item list-group-item-action">Consultar mantenimientos</a>
-        <a href="ListaRutas" class="list-group-item list-group-item-action">Consultar rutas</a>
-        <a href="ListaViajes" class="list-group-item list-group-item-action">Consultar viajes</a>
+        <a href="ListaConductores" class="list-group-item list-group-item-action color text-light">Consultar conductores</a>
+        <a href="ListaBuses" class="list-group-item list-group-item-action color text-light">Consultar buses</a>
+        <a href="ListaMantenimientos" class="list-group-item list-group-item-action color text-light">Consultar mantenimientos</a>
+        <a href="ListaRutas" class="list-group-item list-group-item-action color text-light">Consultar rutas</a>
+        <a href="ListaViajes" class="list-group-item list-group-item-action color text-light">Consultar viajes</a>
     </div>
         </div>
+    </div>
     </div>
 
 
